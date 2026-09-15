ba3714530d60571eb6cd7bbbfae30646db0276de
c_clemma <clara.wangen@web.de>
2026-09-15 23:41:12 +0200

    add path-execution

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -10,8 +10,8 @@ int tokenizer(char *buffer, char *args[], int max_args)
 {
     int arg_count = 0;
 
-    char *cursor = buffer;
-    char *token;
+    char *cursor = buffer; // start at beginning before going to next sperator
+    char *token;           // token is a pointer to a char
 
     while ((token = strsep(&cursor, " \t\n")) != NULL)
     {
@@ -28,6 +28,28 @@ int tokenizer(char *buffer, char *args[], int max_args)
     return arg_count;
 }
 
+char *search_path(char *command)
+{
+    char *path = malloc(100);
+    strcpy(path, "/bin/");
+    strcat(path, command);
+    if (access(path, X_OK) == 0)
+    {
+        return path;
+    }
+    strcpy(path, "/bin/usr");
+    strcat(path, command);
+
+    if (access(path, X_OK) == 0)
+    {
+        return path;
+    }
+    free(path);
+    return NULL;
+}
+
+// printf("%s\n", path);
+
 void exec_command(char *args[])
 {
     int rc = fork();
@@ -42,7 +64,15 @@ void exec_command(char *args[])
         // child (new process)
         // printf("hello, I am child that will execute that command '%s' in the future\n", token);
         // TODO get_path();
-        execv("/usr/bin/ls", args);
+        // exec(args[0], args)
+        // path: need to add args[0];
+        char *command = search_path(args[0]);
+        if (command == NULL)
+        {
+            fprintf(stderr, "Command not found\n");
+            return;
+        }
+        execv(command, args);
         exit(1);
     }
     else
@@ -54,6 +84,7 @@ void exec_command(char *args[])
 
 int main(void)
 {
+    // cause getline allocates memory dynamically
     char *buffer = NULL;
     size_t bufsize = 0;
 
@@ -67,15 +98,17 @@ int main(void)
         // printf("You typed: %s\n", buffer);
 
         // if end-of-file marker : exit(0);
+        // EOF = a signal or condition that tells a computer program no more data remains in a file or input stream
+        // fail or eof the getline() retruns -1
         if (characters == -1)
         {
-            free(buffer);
+            free(buffer); // free the memory after all (see man page)
             exit(0);
         }
 
-        char *args[100];
-
-        int arg_count = tokenizer(buffer, args, 100);
+        char *args[100]; // array 100 elements, that each are a pointer to a char
+        // to split in tokens, need: buffer, args and the limit of 100
+        int arg_count = tokenizer(buffer, args, 100); // amount of tokens
 
         // empty command
         if (arg_count == 0)
